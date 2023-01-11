b1e95f2eec90afe66d773ea0129d9fe1124054a6
Tycho Luyben <tycho@e-lab.nl>
2023-01-11 22:12:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { useState, useMemo } from 'react';
+import Chat from './Chat'
+
+
+function App() {
+
+  return (
+
+    <div className="App">
+      <Chat />
+    </div>
+
+
+  );
+}
+
+export default App;
